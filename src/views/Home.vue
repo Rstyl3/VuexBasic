41db8129390debf10361b32c10eae0a1dea90d93
Severin Haselmann <32898792+Rstyl3@users.noreply.github.com>
2018-09-28 09:54:18 +0200

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+     <product-list-one :products="products" ></product-list-one>
+     <product-list-two :products="products" ></product-list-two>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProductListOne from '@/components/ProductListOne.vue'
+import ProductListTwo from '@/components/ProductListTwo.vue'
+
+export default {
+  name: 'home',
+  components: {
+    ProductListOne,
+    ProductListTwo
+  },
+  props: ['products'],
+  data(){
+    return{
+    }
+  }
+
+}
+</script>
